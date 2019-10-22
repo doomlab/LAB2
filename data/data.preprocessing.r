8dbdd46c06960389ab6f5abb3d70165fb3d9a5ee
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2019-10-22 16:02:36 -0400

    updates and stuff

--- data/data.preprocessing.r
+++ data/data.preprocessing.r
@@ -15,7 +15,7 @@ str(data)
 small_data <- data[ , c("KEYWORDS", "TITLE", "ABSTRACT", "code")]
 small_data$text <- paste(small_data$KEYWORDS, small_data$TITLE, small_data$ABSTRACT, sep = " ")
 small_data$class <- as.numeric(as.factor(small_data$code))
-small_data$class <- ifelse(class == 1, 0, 1)
+small_data$class <- ifelse(small_data$class == 1, 0, 1)
 table(small_data$class)
 
 ## [2] - Data Pre-processing...
@@ -27,6 +27,11 @@ small_data$text <- removeWords(small_data$text, stopwords(kind = "SMART"))
 small_data$text <- removePunctuation(small_data$text)
 small_data$text <- removeNumbers(small_data$text)
 small_data$text <- stripWhitespace(small_data$text)
+
+##write out one here
+write.csv(small_data2, "data/output_data/training_test_data_cleaned_no_stem.csv")
+
+##the stem
 small_data$text <- stemDocument(small_data$text)
 
 write.csv(small_data, "data/output_data/training_test_data_cleaned.csv")
@@ -38,7 +43,7 @@ str(data2)
 small_data2 <- data2[ , c("KEYWORDS", "TITLE", "ABSTRACT", "code")]
 small_data2$text <- paste(small_data2$KEYWORDS, small_data2$TITLE, small_data2$ABSTRACT, sep = " ")
 small_data2$class <- as.numeric(as.factor(small_data2$code))
-small_data2$class <- ifelse(class == 1, 0, 1)
+small_data2$class <- ifelse(small_data2$class == 1, 0, 1)
 table(small_data2$class)
 
 ## [2] - Data Pre-processing...
@@ -50,6 +55,11 @@ small_data2$text <- removeWords(small_data2$text, stopwords(kind = "SMART"))
 small_data2$text <- removePunctuation(small_data2$text)
 small_data2$text <- removeNumbers(small_data2$text)
 small_data2$text <- stripWhitespace(small_data2$text)
+
+##write out one here
+write.csv(small_data2, "data/output_data/new_data_cleaned_no_stem.csv")
+
+##then stem
 small_data2$text <- stemDocument(small_data2$text)
 
 write.csv(small_data2, "data/output_data/new_data_cleaned.csv")
